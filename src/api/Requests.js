5edd581dc87433bde12bae45648d8790c9ab6d91
Fede <fedejmartinez@gmail.com>
2022-09-07 17:09:23 -0300

    Agrega datos en el home

--- src/api/Requests.js
+++ src/api/Requests.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+const getToken = () => ({
+    headers: {
+
+    }
+})
+
+const getCompetitions = () => {
+    return axios.get("http://localhost:8080/competitions", getToken())
+}
+
+export default {getCompetitions}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Home.css"
+import api from "../api/Requests"
 import Navbar from "./Navbar";
 
 const Home = () => {
+
+    const [competitionsData, setCompetitionsData] = useState({
+        competitions: []
+    })
+
+    const navigate = useNavigate(); 
+
+    const updateCompetitions = () => {
+        api.getCompetitions()
+        .then((response) => {
+            setCompetitionsData({
+                competitions: response.data
+            })
+        })
+        .catch((error) => {console.log(error)})
+    }
+
+    const competitionMatches = (id) => {
+        navigate("/matches");
+    };
+
+    useEffect(() => {
+        updateCompetitions();
+    },[]);
 
     return (
         <>  
             <header>
                 <Navbar/>
             </header>
-            <div className="Home">
-                Home
+            <div class="home">
+            { competitionsData.competitions.length > 0 &&
+                (<div class="competitions">
+                    {competitionsData.competitions.map(competition =>{
+                        return(
+                        <span>
+                            <figure class="competition" onClick={() => competitionMatches(competition.code)}>
+                                <img src={competition.emblem} class="comppic" alt={competition.name}/>
+                                <figcaption>{competition.name}</figcaption>
+                            </figure>
+                        </span>
+                        )
+                    })}
+                </div>
+                )}
             </div>
         </>
     )
